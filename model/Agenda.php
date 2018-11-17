3d5f16f892a099e5097be9f16b3555f6d0324a48
Thais <thais_thaina2@hotmail.com>
2018-11-16 22:58:42 -0200

    Tabela de agenda e os CRUDS da agenda

---
+++ model/Agenda.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once('Conexao.php');
+
+class Agenda {
+
+    Private $idagenda;
+    Private $titulo;
+    Private $data;
+    Private $hora;
+	Private $descricao;
+	Private $local;
+    Private $id_users;
+
+    public function cadastrar($titulo, $data, $hora, $descricao, $local, $id_users){
+        $insert = "Insert into agenda(titulo, data, hora, descricao, local, id_users) 
+        values ('$titulo', '$data', '$hora', '$descricao', '$local', '$id_users')";
+			
+        $Acesso = new Acesso();
+
+        $Acesso->Conexao();
+
+        $Acesso->Query($insert);
+    }
+
+    public function listar($sql) {
+
+        $Acesso = new Acesso();
+
+        $Acesso->Conexao();
+
+        $Acesso->Query($sql);
+
+        $this->Linha = @mysqli_affected_rows($Acesso->result);
+
+        $this->Result = $Acesso->result;
+    }
+    
+    public function alterar($titulo, $data, $hora, $descricao, $local, $idagenda) { 
+
+        $update = "update agenda set titulo='$titulo', data='$data', hora='$hora', descricao='$descricao',
+        local='$local' where idagenda='$idagenda')";
+			
+        $Acesso = new Acesso();
+
+        $Acesso->Conexao();
+
+        $Acesso->Query($update);
+    }
+
+    public function excluir($idagenda) { 
+
+        $delete = "delete from agenda where idagenda='$idagenda')";
+			
+        $Acesso = new Acesso();
+
+        $Acesso->Conexao();
+
+        $Acesso->Query($delete);
+    }
+
+}
--- tabelas.sql
+++ tabelas.sql
@@ -2,3 +2,7 @@ CREATE DATABASE AgendaDB
 CREATE TABLE users(id int not null PRIMARY KEY AUTO_INCREMENT, nome varchar(100) not null, 
                     username varchar(50) not null, senha varchar(50) not null, sexo varchar(1) not null,
                     data_nascimento date, telefone varchar(15), email varchar(50), foto varchar(500))
+
+CREATE TABLE agenda(idagenda int not null PRIMARY KEY AUTO_INCREMENT, titulo varchar(20) not null, 
+                    data date not null, hora time, descricao varchar(500), local varchar(50), 
+                    id_users int not null, FOREIGN KEY (id_users) REFERENCES users (id))
