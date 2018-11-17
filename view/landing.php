c46558614e1161890ad74e6feb4fc8cd71a95ead
GustavoCarvalhoPessoa <gustavocarvalhopessoa@hotmail.com>
2018-11-16 23:36:15 -0200

    Landing Page

---
+++ view/landing.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Agenda</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Jumbotron Header -->
+      <header class="jumbotron my-4">
+        <h1 class="display-3">Conheça a equipe!</h1>
+        <p class="lead">Neste projeto a equipe criou um site de Agenda de Contatos aproveitando todo conteúdo de todas as aulas de Linguagem de Programação IV-INTERNET, e também utilizando-se do Bootstrap para melhor performance e layout das páginas.</p>
+        <a href="https://github.com/EduardoSimao/Projeto_LP4" class="btn btn-primary btn-lg">Projeto no GitHub</a>
+      </header>
+
+      <!-- Page Features -->
+      <div class="row text-center">
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <img class="card-img-top" src="https://avatars3.githubusercontent.com/u/37407940?s=400&v=4" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Thaís Lopes</h4>
+              <p class="card-text">Criadora de lindíssimas páginas de gato pela rede, assume novos desafios a cada projeto. Função: Back-end.</p>
+            </div>
+            <div class="card-footer">
+              <a href="https://github.com/Tclsantos09" class="btn btn-primary">Link GitHub</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <img class="card-img-top" src="https://avatars1.githubusercontent.com/u/42583282?s=400&v=4" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Gustavo Carvalho</h4>
+              <p class="card-text">Desenvolvedor gatinho, análise boa, mas não muito veloz, segue o lema: devagar e sempre. Função: Front-End.</p>
+            </div>
+            <div class="card-footer">
+              <a href="https://github.com/GustavoCarvalhoPessoa" class="btn btn-primary">Link GitHub</a>
+            </div>
+          </div>
+        </div>
+
+         <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <img class="card-img-top" src="https://avatars1.githubusercontent.com/u/41209185?s=400&v=4" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Eduardo Simão</h4>
+              <p class="card-text">Programador conceituado na área de TI, requisitado, afiado como a espada de um samurai. Função: Back-End.</p>
+            </div>
+            <div class="card-footer">
+              <a href="https://github.com/EduardoSimao" class="btn btn-primary">Link GitHub</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <img class="card-img-top" src="https://avatars2.githubusercontent.com/u/37520274?s=400&v=4" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Victoria Alícia</h4>
+              <p class="card-text">Desenvolvedora de sites maravilhosos, sempre podendo atender as diversas demandas da paulista. Função: Front-End</p>
+            </div>
+            <div class="card-footer">
+              <a href="https://github.com/rodovalhovic" class="btn btn-primary">Link GitHub</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+ 
+
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
