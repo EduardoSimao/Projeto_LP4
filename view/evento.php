0410a7ca0ccf603b243c7161c059b7a6a61a40a8
Tclsantos09 <thais_thaina2@hotmail.com>
2018-11-19 15:47:24 -0200

    Pagina teste de  evento

---
+++ view/evento.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<?php
+require_once('../controller/controllerAgenda.php');
+Processo('incluir');
+
+?>
+<html>
+<head>
+    <title></title>    
+    <script src="js/Validar.js"></script>
+    <?php include("bootstrap.php"); ?>
+</head>
+<body>
+
+<header>
+    <?php include("menu.php"); ?>
+<header>
+    <div class="container">
+        <h1 class="well">Cadastro de Eventos</h1>
+        <div class="col-lg-12 well">
+            <div class="row">
+                <form action="" name="form" method="POST">
+                    <div class="col-sm-12">
+                        <div class="row">
+                            <div class="col-sm-6 form-group">
+                                <label>Titulo</label>
+                                <input type="text" placeholder="Insira o titulo do evento" class="form-control" name="titulo">
+                            </div>                            
+                            <div class="col-sm-6 form-group">
+                                <label>Data</label>
+                                <input type="date" class="form-control" name="data">
+                            </div>                            
+                            <div class="col-sm-6 form-group">
+                                <label>Hora</label>
+                                <input type="time" class="form-control" name="hora">
+                            </div>			
+                                                   
+                            <div class="col-sm-6 form-group">
+                                <label>Local</label>
+                                <input type="text" class="form-control" name="local">
+                            </div>
+                            	
+                            <div class="col-sm-12 form-group">
+                                <label>Descrição do Evento</label>
+                                <input type="text" placeholder="Descreva o evento" class="form-control " name="descricao" >
+                            </div>
+                        </div>
+                        
+                        <button type="button" class="btn btn-block btn-info" onclick="validar(document.form);">Cadastrar</button> 					
+                        <input type="hidden" name="ok" id="ok" />   
+                    </div>
+                </form> 
+            </div>
+        </div>
+    </div>
+</body>
+
+  <footer class="">
+    <?php include("footer.php"); ?>
+  </footer>
+</html>
