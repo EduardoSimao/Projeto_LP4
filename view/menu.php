0c7b6d67308596c60e43e253364547cd42fab648
GustavoCarvalhoPessoa <gustavocarvalhopessoa@hotmail.com>
2018-11-19 21:16:08 -0200

    ajuste no botão compromissos

--- view/menu.php
+++ view/menu.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
       <div class="navbar-header">
-        <a class="navbar-brand" href="#">Compromissos</a>
+        <a class="navbar-brand" href="homepage.php">Compromissos</a>
       </div>
 <?php 
 
